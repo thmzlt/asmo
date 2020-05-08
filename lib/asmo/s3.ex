840c1d8fa35ad5dd0467ea57b8283a618b913050
Thomaz Leite <git@thomazleite.com>
2020-05-07 22:51:23 -0300

    Implement basic S3 operations

---
+++ lib/asmo/s3.ex
@@ -0,0 +1,28 @@
+defmodule Asmo.S3 do
+  @moduledoc false
+
+  @spec copy(String.t(), String.t(), String.t(), String.t()) :: {:ok, term()}
+
+  def copy(dest_bucket, dest_key, src_bucket, src_key) do
+    request = ExAws.S3.put_object_copy(dest_bucket, dest_key, src_bucket, src_key)
+
+    {:ok, _res} = ExAws.request!(request)
+  end
+
+  @spec delete(String.t(), String.t()) :: {:ok, term()}
+
+  def delete(bucket, key) do
+    request = ExAws.S3.delete_object(bucket, key)
+
+    {:ok, _res} = ExAws.request!(request)
+  end
+
+  @spec upload(String.t(), String.t(), String.t()) :: {:ok, term()}
+
+  def upload(bucket, key, path) do
+    data = File.read!(path)
+    request = ExAws.S3.put_object(bucket, key, data)
+
+    {:ok, _res} = ExAws.request(request)
+  end
+end
